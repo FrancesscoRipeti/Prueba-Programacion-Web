11688bcccd6ca6b5321032818d7abdea973882df
FrancesscoRipeti <fr.ripeti@duocuc.cl>
2024-05-17 18:49:12 -0400

    aaaa
    ini-reg-sesión
    
    recuerda que falta arreglar el inicio sesion y los botones reg y ini ....
    ademas de que falta validar con jquery

---
+++ script.js
@@ -0,0 +1,18 @@
+/* inicio sesion */
+const btnSingIn = document.getElementById('btn-sing-in');
+const btnSingUP = document.getElementById('btn-sign-up');
+const container = document.querySelector("#container-ini-reg");
+
+btnSingIn.addEventListener('click', () => {
+    container.classList.toggle('toggle');
+}
+);
+btnSingUP.addEventListener('click', () => {
+    container.classList.toggle('toggle');
+}
+);
+
+btn.addEventListener('click', () => {
+    container.classList.toggle('toggle');
+});
+
